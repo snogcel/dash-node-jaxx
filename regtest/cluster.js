a95fdb23b8ffa4acaa0f70b80f4b5ffcb8c25d7a
snogcel <ledniknoj@gmail.com>
2016-06-02 21:19:40 -0600

    dashify daemon references
    fix create dependencies
    
    fix build issue
    
    use bitcore-lib-dash
    
    correct config file name for test modes
    
    correct 'dashd' daemon name in error message
    
    Makes message less confusing in debug situations

--- regtest/cluster.js
+++ regtest/cluster.js
@@ -6,7 +6,7 @@ var spawn = require('child_process').spawn;
 
 var BitcoinRPC = require('bitcoind-rpc');
 var rimraf = require('rimraf');
-var bitcore = require('bitcore-lib');
+var bitcore = require('bitcore-lib-dash');
 var chai = require('chai');
 var should = chai.should();
 
